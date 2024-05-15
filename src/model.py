e486fb047adbbc7bc4c2bf80360e568560e657ac
Eric Xu <xydxydxyd1@gmail.com>
2024-05-15 16:00:24 -0700

    Filter out recognized column and update model py

--- src/model.py
+++ src/model.py
@@ -1,13 +1,14 @@
-# Train and testing model
+# Data loading
 
 import pandas as pd
 
-def load_ndjson_to_df(file_names):
+def load_ndjson_to_df(file_names, print_progress=False):
     """
     Load one or more ndjson files into a pandas DataFrame.
 
     Args:
         file_names (list or str): A list of ndjson file names or a single file name.
+        print_progress: if True, prints debug messages that shows the progress
 
     Returns:
         pandas.DataFrame: A DataFrame containing the data from the ndjson file(s).
@@ -25,10 +26,14 @@ def load_ndjson_to_df(file_names):
         # Iterate over the list of file names
         for file_name in files:
             # Read the ndjson file into a DataFrame
+            if (print_progress):
+                print(f"Reading {file_name}...", end=" ")
             df = pd.read_json(file_name, lines=True, orient='records')
 
             # Concatenate the DataFrame with the combined DataFrame
             combined_df = pd.concat([combined_df, df], ignore_index=True)
+            if (print_progress):
+                print("Completed")
 
         return combined_df
 
@@ -37,9 +42,24 @@ def load_ndjson_to_df(file_names):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
+data_files = ["./data/full_simplified_apple.ndjson",
+                "./data/full_simplified_banana.ndjson",
+                "./data/full_simplified_blueberry.ndjson",
+                "./data/full_simplified_watermelon.ndjson",]
+print("Loading data...")
+df = load_ndjson_to_df(data_files, print_progress=True)
+print(df);
+
+# EDA
+print("Preprocessing data...")
+
 def clean_ndjson_data(df):
     """
-    Clean up the ndjson data by removing the "countrycode", "timestamp", and "key_id" columns.
+    Clean up the ndjson data by removing metadata columns and filtering out unrecognized
+    data
+    
+    Metadata columns removed: "countrycode", "timestamp", and "key_id" columns.
     
     Args:
         df (pandas.DataFrame): The DataFrame containing the ndjson data.
@@ -48,16 +68,12 @@ def clean_ndjson_data(df):
         pandas.DataFrame: A new DataFrame with the specified columns removed.
     """
     # Drop the specified columns
-    cleaned_df = df.drop(columns=["countrycode", "timestamp", "key_id"])
-    
+    cleaned_df = df[df['recognized'] == True]
+    cleaned_df = cleaned_df.drop(columns=["countrycode", "timestamp", "key_id", "recognized])
+
     return cleaned_df
 
-data_files = ["./data/full_simplified_apple.ndjson",
-                "./data/full_simplified_banana.ndjson",
-                "./data/full_simplified_blueberry.ndjson",
-                "./data/full_simplified_watermelon.ndjson",]
-
-df = load_ndjson_to_df(data_files)
-df = clean_ndjson_data(df)
-df = pd.get_dummies(df, columns=["word"], prefix=["fruit"])
-print(df);
+cleaned_df = clean_ndjson_data(df)
+# One-hot encode "word"
+cleaned_df = pd.get_dummies(cleaned_df, columns=["word"], prefix=["fruit"])
+print(cleaned_df);
